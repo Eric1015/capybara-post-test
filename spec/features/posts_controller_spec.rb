861e4cd613fea70373ecb30316d7b8d05bb2c190
Eric1015 <love1dbsb77@hotmail.com>
2018-07-24 12:36:18 -0700

    Made the capybara test about post and made sure it passed

---
+++ spec/features/posts_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+RSpec.feature "adding post" do
+    scenario "allow a user to add a post" do
+        visit posts_new_path
+
+        fill_in "Title", with: "Rspec Introduction"
+        fill_in "Body", with: "This is the first post about Rspec"
+
+        click_on("Create Post")
+
+        expect(page).to have_content("Rspec Introduction")
+        expect(page).to have_content("This is the first post about Rspec")
+    end
+end
